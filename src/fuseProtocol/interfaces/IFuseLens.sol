a7b29ff224edede703da70e9e7b15fa853928794
roseiliend <roseiliend@gmail.com>
2023-09-06 20:14:40 +0800

    remove fuseProtocolExpolit interface

--- src/fuseProtocol/interfaces/IFuseLens.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity ^0.8.10;
-interface IFuseLens {
-    function getPoolUserSummary(address comptroller, address account)
-        external
-        returns (uint256, uint256);
-}
--- src/fuseProtocol/interfaces/IWETH.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity ^0.8.10;
-
-interface IWETH {
-    function withdraw(uint wad) external;
-}
