384e233eb78f2873d947f3c3fbe49cbfd33c831e
dimitry <dimitry@0xHacked.com>
2023-10-26 20:44:41 +0800

    add SamplePoC.sol

---
+++ src/SamplePoC.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.4.22 <0.9.0;
+
+// This `Exploit` contract is a must-have. zkProver invokes Exploit contract by default.
+contract Exploit {
+    // constructor functions aren't supported.
+    // constructor() {
+    //    code here doesn't work.
+    // }
+
+    function exploit() public {}
+}
